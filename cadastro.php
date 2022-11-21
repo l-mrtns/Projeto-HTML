b0c8bf2f6fea472d900cdc077300d881731d6bed
l-mrtns <114369038+l-mrtns@users.noreply.github.com>
2022-11-21 15:08:46 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -3,16 +3,16 @@ include_once 'conecta.php';
 
 $nome = $_POST['nome'];
 $email = $_POST['email'];
-$telefone = $_POST['telefone'];
+$senha = $_POST['senha'];
 
 try {
-    $stm = $conn->prepare("INSERT INTO usuario (nome, email, telefone) VALUES (:nome, :email, :telefone)");
+    $stm = $conn->prepare("INSERT INTO usuario (nome, email, senha) VALUES (:nome, :email, :senha)");
     $stm->bindParam(':nome', $nome);
     $stm->bindParam(':email', $email);
-    $stm->bindParam(':telefone', $telefone);
+    $stm->bindParam(':senha', $senha);
     $stm->execute();
     echo "Cadastro efetuado!<br>";
 } catch (Exception $e) {
-    echo $sql . "<br>" . $e->getMessage() . "<br>";
+    echo $sql . "<br>" . $e.getMessage() . "<br>";
 }
 ?>
